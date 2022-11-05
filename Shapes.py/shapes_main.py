2748794432c95a59c0b4cc53523b80bc3cc789ce
Saidi Mukasa <101624934+Saidimukasa@users.noreply.github.com>
2022-11-05 12:24:33 +0300

    Update shapes_main.py

--- Shapes.py/shapes_main.py
+++ Shapes.py/shapes_main.py
@@ -1,6 +1,9 @@
-from Shapes_area_and_perimeter import *
+# Importing properties from the Shapes Files or Modules 
+from Shapes_area_and_perimeter import * # We can use (*) to show that we are importing all
 def main():
+#     The Choices Function being Evoked 
     Choice()
+#     Input loop
     while True:
         print()
         choice = input("Do you want to continue? (y(yes)/n(no)): ")
